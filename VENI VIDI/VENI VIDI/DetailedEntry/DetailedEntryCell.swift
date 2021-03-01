bd867211602bdfd00eba3ee1a7bec2e97257bae7
Xiaowen Ma <maxiaowen@wustl.edu>
2021-03-02 00:05:48 +0800

    updareDetailedCell

--- VENI VIDI/VENI VIDI/DetailedEntry/DetailedEntryCell.swift
+++ VENI VIDI/VENI VIDI/DetailedEntry/DetailedEntryCell.swift
@@ -64,18 +64,19 @@ class DetailedEntryCell:DCCell<DetailedEntryCellModel>{
         let left: CGFloat = 15
 
 
-        //titleLabel.frame = CGRect(x: left, y: 15, width: bounds.width-30, height: 50)
+        
         poster.frame = CGRect(x: left, y: 15, width: bounds.width-30, height: 150)
         //separateLine.frame = CGRect(x: left, y: bounds.height - height, width: bounds.width - left, height: height)
         smallPoster.frame = CGRect(x: left, y: 180, width: 100, height: 150)
         stars.frame = CGRect(x: left+115, y: 300, width: bounds.width-145, height: 30)
+        titleLabel.frame = CGRect(x: left+115, y: 180, width: bounds.width-145, height: 30)
         comment.frame=CGRect(x: left, y: 345, width: bounds.width-30, height: 350)
 
     }
     
     override func cellModelDidUpdate() {
         super.cellModelDidUpdate()
-        //label.text = cellModel.timeLabel
+        titleLabel.text = cellModel.entryTitle
         titleLabel.text=cellModel.entryTitle
         poster.image=cellModel.posterImage
         smallPoster.image=cellModel.posterImage
