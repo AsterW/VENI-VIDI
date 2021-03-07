a51104b4a5e0ef7a2626cace206c5e8dfb77ba61
Will M <xiaoteng.ma@wustl.edu>
2021-03-06 18:28:48 -0600

    Add data module

---
+++ VENI VIDI/VENI VIDI/Data/JournalEntry+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  JournalEntry+CoreDataClass.swift
+//  
+//
+//  Created by 雲無心 on 3/6/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(JournalEntry)
+public class JournalEntry: NSManagedObject {
+
+}
---
+++ VENI VIDI/VENI VIDI/Data/Tag+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Tag+CoreDataClass.swift
+//  
+//
+//  Created by 雲無心 on 3/6/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Tag)
+public class Tag: NSManagedObject {
+
+}
