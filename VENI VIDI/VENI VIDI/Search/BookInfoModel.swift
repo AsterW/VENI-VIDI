b3b445c4155e1b6da00799ee00e90ff210fbb223
MonAster <asterw0616@gmail.com>
2021-03-16 00:09:10 +0800

    VENI VIDI: set up API.

---
+++ VENI VIDI/VENI VIDI/Search/BookInfoModel.swift
@@ -0,0 +1,17 @@
+//
+//  BookInfoModel.swift
+//  VENI VIDI
+//
+//  Created by MonAster on 2021/3/15.
+//
+
+import Foundation
+
+struct EntryInfo: Decodable {
+    let id: Int
+    let bookInfo: BookInfo
+}
+
+struct BookInfo: Decodable {
+    
+}
